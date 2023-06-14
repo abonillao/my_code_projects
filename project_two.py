a009b85c8287996a110f7d2d98f86d881beba7a2
Andres Bonilla <cabonillao.oc@gmail.com>
2023-06-14 16:42:38 -0500

    add project two file

---
+++ project_two.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+'''
+@File    :   project_two.py
+@Time    :   2023/06/14 16:42:04
+@Author  :   Andres Bonilla 
+@Version :   1.0
+@Contact :   cabonillao.oc@gmail.com
+@License :   (C)Copyright 2023-2024, Andres Bonilla
+@Desc    :   None
+'''
+
+import numpy as np
+import pandas as pd
